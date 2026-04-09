4d72793748d94ca01be498c28c1098b920c3247e
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-04-10 00:20:29 +0530

    Update import.py

--- fundamentals/import.py
+++ fundamentals/import.py
@@ -1,3 +1,18 @@
+# Program to display calendar of the given month and year
+import calendar
+yy = int(input("Enter year: "))
+mm = int(input("Enter month: "))
+print(calendar.month(yy, mm))
+# output
+#    November 2014
+# Mo Tu We Th Fr Sa Su
+#              1  2
+# 3  4  5  6 7  8  9
+# 10 11 12 13 14 15 16
+# 17 18 19 20 21 22 23
+# 24 25 26 27 28 29 30
+
+
 # Example 1: Using pathlib.Path.mkdir
 from pathlib import Path
 Path("/root/dirA/dirB").mkdir(parents=True, exist_ok=True)
