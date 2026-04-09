f479b12180cde23f28677597e3a7cfbfaa13d474
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-04-10 00:11:43 +0530

    Update variables.py

--- fundamentals/variables.py
+++ fundamentals/variables.py
@@ -1,3 +1,11 @@
+# Python Program to Convert Decimal to Binary, Octal and Hexadecimal
+# Python program to convert decimal into other number systems
+dec = int(input("Enter No."))
+print("The decimal value of", dec, "is:")
+print(bin(dec), "in binary.")
+print(oct(dec), "in octal.")
+print(hex(dec), "in hexadecimal.")
+
 a = 10          # int
 b = 3.14        # float
 c = "Ankit"     # string
