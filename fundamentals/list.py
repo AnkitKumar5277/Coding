01b219f4c68e4b6afd9dc627deff077823c021ea
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-04-20 16:09:03 +0530

    Update list.py

--- fundamentals/list.py
+++ fundamentals/list.py
@@ -1,3 +1,10 @@
+# write a program to print third fifthe and seventh element from a list using enurmerate function
+'''
+l = [1,2,3,4,5,6,7,8,9,10]
+for index, item in enumerate(l):
+  if index == 2 or index == 4 or index == 6:
+     # print(index, item)
+    print(f"the{index + 1}th element is {item}")
 
 filter(function, iterable)
 
