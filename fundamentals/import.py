86213fa5f82ef2918b56fcb2f72a361e765c0f80
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-04-24 22:27:08 +0530

    Update import.py

--- fundamentals/import.py
+++ fundamentals/import.py
@@ -2,6 +2,16 @@
 
 # type virtualenv ak in shell or terminal
 
+# explore the flask module and create a web server using flask & python.
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+  return 'Hello, World!'
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 import keyword
 print(keyword.kwlist)
