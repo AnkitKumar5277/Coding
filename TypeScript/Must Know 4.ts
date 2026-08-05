bf1a1a7a8772d94788d4e862d1d15f49ddd92ee1
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-08-05 10:42:17 +0530

    Update Must Know 4.ts

--- TypeScript/Must Know 4.ts
+++ TypeScript/Must Know 4.ts
@@ -2,6 +2,29 @@
 let name = "Ankit";
 console.log(name); // Ankit
 
+// Interface - Can be used to make the parameters cleaner and reusable
+interface User {
+ name: string;
+ email: string;
+}
+const user: User = {
+ name: "Ankit",
+ email: "ankit@test.com"
+};
+
+// Class
+class Employee {
+ name: string;
+ constructor(name: string) {
+   this.name = name;
+ }
+ display() {
+   console.log(this.name);
+ }
+}
+const emp = new Employee("Ankit");
+emp.display();
+
 // Arrow functions 
 const add = (a:number, b:number): number => {
   return a +b;
